e3efad3282133d0be29f7476476af1a65f09fd15
Ahmed Saad <ask@shopbox.com>
2018-11-04 00:19:07 +0200

    showing all packets

--- packet_logging/views.py
+++ packet_logging/views.py
@@ -6,7 +6,7 @@ from elasticsearch import Elasticsearch
 import os
 import signal
 import subprocess
-from packet_sniffer_log_in_es.settings import index_name , type_name
+from packet_sniffer_log_in_es.settings import index_name, type_name
 
 
 '''
@@ -22,21 +22,18 @@ def packet_sniffer_logger(request):
 def search(request):
     es = Elasticsearch()
     if request.method == 'POST': # if the search form is submitted
-        filters_list = []
+        filters_dic = {}
         # loop on each search param and check if it has value to add it to filter list
         for param in ["version","ip_header_length","ttl","protocol","source_address","destination_address",
             "source_port","dest_port","sequence_number", "acknowledgement", "tcp_header_length", "data", "datetime"]:
             if request.POST.get(param) != '':
-                q_param = TermFilter(param, request.POST.get(param))
-                filters_list.append(q_param)
-        if len(filters_list) != 0: # if there is filter params  get the results
-            orq = ANDFilter(filters_list)
-            q = FilteredQuery(MatchAllQuery(), orq)
-            data = es.search(index=index_name, body={"query": q})
+                filters_dic[param] = request.POST.get(param)
+        if len(filters_dic) != 0: # if there is filter params  get the results
+            data = es.search(index=index_name, doc_type=type_name, body={"query": {"match": filters_dic}})
         else:
             data = None
     elif request.method == 'GET': # get all packet when get the search page
-        data = es.search(index=index_name, body={"query": {"match_all": {}}})
+        data = es.search(index=index_name, doc_type=type_name, body={"query": {"match_all": {}}})
     return render(request, 'search.html',{'log_results':data['hits']['hits']})
 
 
